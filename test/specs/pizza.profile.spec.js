8bca6b30ff6794fd6965c817316ba7d1729a6869
josdem <joseluis.delacruz@gmail.com>
2021-11-04 08:54:09 -0400

    #1 Formatting code

--- test/specs/pizza.profile.spec.js
+++ test/specs/pizza.profile.spec.js
@@ -1,11 +1,9 @@
-const assert = require('assert')
+const assert = require("assert")
 
-describe('Loading Dominos webpage', () => {
-
-   it("validates website title", async() => {
-       await browser.url("https://webdriver.io/")
-       const title = await browser.getTitle()
-       assert.strictEqual(title, "WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO")
-   })
-
+describe("Loading Dominos webpage", () => {
+  it("validates website title", async () => {
+    await browser.url("https://webdriver.io/")
+    const title = await browser.getTitle()
+    assert.strictEqual(title, "WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO")
+  })
 })

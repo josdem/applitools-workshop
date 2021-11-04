ff409a50ea817653ccaa0f02b9852d6cd09311e1
josdem <joseluis.delacruz@gmail.com>
2021-11-04 08:41:24 -0400

    Fixing first test case

---
+++ test/specs/pizza.profile.spec.js
@@ -0,0 +1,11 @@
+const assert = require('assert')
+
+describe('Loading Dominos webpage', () => {
+
+   it("validates website title", async() => {
+       await browser.url("https://webdriver.io/")
+       const title = await browser.getTitle()
+       assert.strictEqual(title, "WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO")
+   })
+
+})

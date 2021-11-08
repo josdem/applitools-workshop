119bb951ab8c693de00e8c56b72a0ba08a476c63
josdem <joseluis.delacruz@gmail.com>
2021-11-08 11:47:07 -0500

    #4 Adding broswer size configuration

--- test/utils/applitools.util.js
+++ test/utils/applitools.util.js
@@ -2,4 +2,20 @@ module.exports = Object.freeze({
   APPLITOOLS_SERVER: "https://eyes.applitools.com/",
   BATCH_INFO: "Ultrafast Batch",
   BREAK_POINT_SIZE: 700,
+  CHROME: {
+    width: 800,
+    height: 600,
+  },
+  FIREFOX: {
+    width: 800,
+    height: 600,
+  },
+  EDGE: {
+    width: 800,
+    height: 600,
+  },
+  SAFARI: {
+    width: 1024,
+    height: 768,
+  },
 })

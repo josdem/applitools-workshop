01848cf210296e7a7ffbed58693e732aab43ab55
josdem <joseluis.delacruz@gmail.com>
2021-11-15 17:29:53 -0500

    #5 Moving Applitools methods to the utils

--- test/utils/applitools.util.js
+++ test/utils/applitools.util.js
@@ -1,21 +1,75 @@
-module.exports = Object.freeze({
-  APPLITOOLS_SERVER: "https://eyes.applitools.com/",
-  BATCH_INFO: "Ultrafast Batch",
-  BREAK_POINT_SIZE: 700,
-  CHROME: {
-    width: 800,
-    height: 600,
-  },
-  FIREFOX: {
-    width: 800,
-    height: 600,
-  },
-  EDGE: {
-    width: 800,
-    height: 600,
-  },
-  SAFARI: {
-    width: 1024,
-    height: 768,
-  },
-})
+const { VisualGridRunner, RunnerOptions, Eyes, Target, BatchInfo, BrowserType, DeviceName, ScreenOrientation } = require("@applitools/eyes-webdriverio")
+
+const APPLITOOLS_SERVER = "https://eyes.applitools.com/"
+const BATCH_INFO = "Ultrafast Batch"
+const BREAK_POINT_SIZE = 700
+const CHROME = {
+  width: 800,
+  height: 600,
+}
+const FIREFOX = {
+  width: 800,
+  height: 600,
+}
+const EDGE = {
+  width: 800,
+  height: 600,
+}
+const SAFARI = {
+  width: 1024,
+  height: 768,
+}
+
+let eyes
+let runner
+let configuration
+
+const setUpConfiguration = async () => {
+  const runnerOptions = new RunnerOptions().testConcurrency(5)
+  runner = new VisualGridRunner(runnerOptions)
+  eyes = new Eyes(runner)
+
+  configuration = eyes.getConfiguration()
+  configuration.setApiKey(process.env.APPLITOOLS_API_KEY)
+  configuration.setServerUrl(APPLITOOLS_SERVER)
+  configuration.setBatch(new BatchInfo(BATCH_INFO))
+  configuration.addBrowser(CHROME.width, CHROME.height, BrowserType.CHROME)
+  configuration.addBrowser(FIREFOX.width, FIREFOX.height, BrowserType.FIREFOX)
+  configuration.addBrowser(EDGE.width, EDGE.height, BrowserType.EDGE_CHROMIUM)
+  configuration.addBrowser(SAFARI.width, SAFARI.height, BrowserType.SAFARI)
+  configuration.addDeviceEmulation(DeviceName.iPhone_11, ScreenOrientation.PORTRAIT)
+  configuration.addDeviceEmulation(DeviceName.Pixel_5, ScreenOrientation.PORTRAIT)
+}
+
+const setUpTest = async (appName, testName) => {
+  configuration.setAppName(appName)
+  configuration.setTestName(testName)
+  eyes.setConfiguration(configuration)
+  await eyes.open(browser)
+}
+
+const checkWindowEyes = async (screenshot) => {
+  await eyes.check(screenshot, Target.window().layoutBreakpoints(BREAK_POINT_SIZE))
+}
+
+const closeEyes = async () => {
+  await eyes.closeAsync()
+}
+
+const cleaning = async () => {
+  await eyes.abortAsync()
+}
+
+const publishing = async () => {
+  const results = await runner.getAllTestResults(false)
+  console.log(results)
+}
+
+module.exports = {
+  setUpConfiguration,
+  setUpTest,
+  checkWindowEyes,
+  closeEyes,
+  cleaning,
+  publishing,
+}

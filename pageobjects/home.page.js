eaa3fad5deb9ab2ece67f9577c7cf0766f39b487
josdem <joseluis.delacruz@gmail.com>
2021-11-16 21:44:03 -0500

    #6 Adding home page

---
+++ pageobjects/home.page.js
@@ -0,0 +1,12 @@
+import { BrowserType } from "@applitools/eyes-webdriverio"
+
+const properties = require(`../properties/${process.env.NODE_ENV}.properties`)
+
+class Home {
+  async open() {
+    await browser.url(properties.url)
+    return await browser.getTitle()
+  }
+}
+
+export const HomePage = new Home()
